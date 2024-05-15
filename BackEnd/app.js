61ee91f4d2e2c4a75ea78b4505ec56a29a778e71
Harsh-Yadav-1312 <119126769+Harsh-Yadav-1312@users.noreply.github.com>
2024-05-16 02:36:10 +0530

    Update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -66,7 +66,7 @@ app.use((error, req, res, next) => {
 mongoose
     .connect(url)
     .then(() => {
-        app.listen(5000);
+        app.listen(process.env.PORT || 5000);
     })
     .catch(err => {
         console.log(err);
